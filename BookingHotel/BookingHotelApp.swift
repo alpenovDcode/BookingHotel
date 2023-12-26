e6d7bccad0fed59d2de24328ca93f3ea3c2cfbf9
alpenovDcode <alpenovevgeniy05@gmail.com>
2023-12-26 23:55:03 +0600

    Initial Commit

---
+++ BookingHotel/BookingHotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingHotelApp.swift
+//  BookingHotel
+//
+//  Created by Альпеша on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingHotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
