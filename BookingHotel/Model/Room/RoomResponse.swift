599548fa1aa756236a5bf936a436c1553a3d8e80
alpenovDcode <alpenovevgeniy05@gmail.com>
2023-12-27 20:50:02 +0600

    Exception "Thread 1: EXC_BAD_ACCESS (code=2, address=0x16cbfbfe0)". Need to debug and solve

---
+++ BookingHotel/Model/Room/RoomResponse.swift
@@ -0,0 +1,18 @@
+//
+//  RoomResponse.swift
+//  BookingHotel
+//
+//  Created by Альпеша on 27.12.2023.
+//
+
+import SwiftUI
+
+struct RoomResponse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoomResponse()
+}
---
+++ BookingHotel/Network/RoomAPI/NetworkRoom.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkRoom.swift
+//  BookingHotel
+//
+//  Created by Альпеша on 27.12.2023.
+//
+
+import SwiftUI
+
+struct NetworkRoom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkRoom()
+}
